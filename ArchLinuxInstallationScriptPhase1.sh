25294808944b48a89ceb1f897e5a5f3899f77576
Nicholas Bell <73562150+NicktotheBell@users.noreply.github.com>
2022-04-29 09:01:36 -0600

    Added comments to scripts

---
+++ ArchLinuxInstallationScriptPhase1.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Arch Linux Installation Script Phase 1
+# By Nicholas Bell
+
+# Set console font to terminus size 16 normal
+echo Setting console font
+setfont ter-116n
+
+# Enable NTP
+echo Enabling NTP
+timedatectl set-ntp true
+
+# Partition the disk
+echo Using cgdisk to partition nvme0n1
+cgdisk /dev/nvme0n1
+
+# Format the partitions, EFI as FAT32, root as BTRFS
+echo Formatting /dev/nvme0n1p1 as FAT32 and /dev/nvme0n1p2 as BTRFS
+mkfs.fat -F 32 /dev/nvme0n1p1
+mkfs.btrfs /dev/nvme0n1p2
+
+# Mount the filesystems, create /boot and mount it
+echo Mounting the filesystems
+mount /dev/nvme0n1p2 /mnt
+mkdir /mnt/boot
+mount /dev/nvme0n1p1 /mnt/boot
+
+# Select the latest 20 HTTPS mirrors in the United States and sort them by rate
+echo Sorting mirrors
+reflector --latest 20 --protocol https --country 'United States' --sort rate --save /etc/pacman.d/mirrorlist
+
+# Install essential packages
+echo Installing essential packages
+pacstrap /mnt base base-devel linux linux-firmware btrfs-progs networkmanager nano man-db man-pages texinfo intel-ucode
+
+# Generate an fstab file
+echo Generating fstab file
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Change root into the new system
+echo Changing root into the new system
+arch-chroot /mnt
---
+++ ArchLinuxInstallationScriptPhase2.sh
@@ -0,0 +1,47 @@
+# Set the time zone and update the system clock
+echo Setting local time zone and updating the system clock
+ln -sf /usr/share/zoneinfo/America/Denver /etc/localtime
+hwclock --systohc
+
+# Edit localization files and generate the locales
+echo Setting up localization
+nano /etc/locale.gen
+locale-gen
+echo LANG=en_US-UTF.8 >> /etc/locale.conf
+
+# Make console font changes persistant
+echo Saving console ofnt changes
+echo consolefont=ter-116n >> /etc/vconsole.conf
+
+# Create the hostname file and setup the hosts file
+echo Creating hostname file and setting up the hosts file
+echo ArchLinuxPC >> /etc/hostname
+nano /etc/hosts
+
+# Enable NetworkManager service
+echo Enabling NetworkManager
+systemctl enable NetworkManager.Service
+
+# Edit and recreate initramfs
+echo Editing intramfs config and recreating initramfs
+nano /etc/mkinitcpio.conf
+mkinitcpio -P
+
+# Create nicholasbell user account and set password
+echo Creating user account
+useradd -mG wheel nicholasbell
+passwd nicholasbell
+
+# Enable sudo for users of wheel group
+echo Enabling sudo
+EDITOR=nano visudo
+
+# Install systemdboot and configure it
+echo Installing and configuring bootloader
+bootctl install
+nano /boot/loader/loader.conf
+nano /boot/loader/entries/arch.conf
+
+# Exit chroot environment
+echo Done
+exit
